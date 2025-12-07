75e86a296a0208b25e2525c961be6505ab2f7d73
Guest <guest-uvnkdq@h24208.sgb.skir.kr>
2025-12-07 22:40:48 +0900

    1947 정답

--- Dynamic_Programming/G3/1947.cpp
+++ Dynamic_Programming/G3/1947.cpp
@@ -5,11 +5,24 @@ using namespace std;
 
 long long cache[1000001];
 long long MOD = 1000000000;
-
 long long present(int n){
     long long& ret = cache[n];
-    if(ret != -1) return n;
+    if(ret != -1) return ret;
     if(n==1) return ret = 0;
     if(n==2) return ret = 1;
+    ret = 0;
+    ret += (n-1)*present(n-1);
+    ret %=MOD;
+    ret += ((n-1)*present(n-2))%MOD;
+    ret %= MOD;
+    return ret;
+}
 
+int main(){
+    int N;
+    cin >> N;
+    for(int i=0;i<=N;i++){
+        cache[i] = -1;
+    }
+    cout << present(N);
 }
