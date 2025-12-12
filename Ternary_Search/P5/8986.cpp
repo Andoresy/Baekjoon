1f06385409ba9449d9014d6c9182f19526e54248
Guest <guest-sprcpx@h24208.sgb.skir.kr>
2025-12-12 22:44:57 +0900

    8986fin

--- Ternary_Search/P5/8986.cpp
+++ Ternary_Search/P5/8986.cpp
@@ -2,3 +2,41 @@
 #include <cmath>
 #include <iomanip>
 using namespace std;
+
+long long loc[100001];
+int N;
+long long cost(long long t){
+    long long ret = 0;
+    for(int i=0;i<N;i++){
+        ret += abs(loc[i]-t*i);
+    }
+    return ret;
+}
+
+int main(){
+    cin >> N;
+    long long maxval = 0;
+    for(int i=0;i<N;i++) {
+        cin >> loc[i];
+        maxval = max(loc[i], maxval);
+    }
+
+    long long left = 1;
+    long long right = maxval;
+    while(left < right-2){
+        long long midl = left + (right-left)/3;
+        long long midr = left + ((right-left)*2)/3;
+        if(cost(midl) < cost(midr)){
+            right = midr;
+        }else{
+            left = midl;
+        }
+        //cout << left << " " <<right << " "<<cost(left)<< endl;
+    }
+    long long minval = cost(left);
+    for(int l = left+1;l<=right;l++){
+        minval = min(cost(l), minval);
+    }
+    cout << minval;
+    
+}
