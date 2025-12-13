b97b9966780f0c60482b74d1fbc5cfb95027633e
Guest <guest-f7ht9k@h24208.sgb.skir.kr>
2025-12-13 15:07:00 +0900

    2075done

--- Priority_Queue/S3/2075.cpp
+++ Priority_Queue/S3/2075.cpp
@@ -3,7 +3,28 @@
 #include <cstring>
 using namespace std;
 
+priority_queue<int> realpq;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
+    int N;
+    cin >> N;
+    for(int i=0;i<N;i++){
+        priority_queue<int> pq;
+        for(int j=0;j<N;j++){
+            int t;
+            cin >> t;
+            realpq.push(t);
+        }
+        for(int i=0;i<N;i++){
+            pq.push(realpq.top());
+            realpq.pop();
+        }
+        realpq = pq;
+    }
+    for(int i=0;i<N-1;i++){
+        realpq.pop();
+    }
+    cout << realpq.top();
 }
